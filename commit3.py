c79f92d1622f256814aa9dcd553254832fb852a4
parsapartovi <pparsapartovi13@gmail.com>
2025-12-05 14:22:01 +0330

    aiming formula

--- commit3.py
+++ commit3.py
@@ -3,9 +3,7 @@ import random
 
 players_name = ["ali", "hamid", "alireza", "nima"]
 
-
 random.shuffle(players_name)
-
 
 team1 = players_name[:2]
 team2 = players_name[2:]
---
+++ commit4.py
@@ -0,0 +1,9 @@
+import math
+x= 5
+y=3
+z = 20
+
+
+aim = x ** 2 + y ** 2 + z *  2
+root = math.sqrt(aim)
+print(root)
