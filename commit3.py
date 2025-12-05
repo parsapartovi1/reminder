52d01e465cbd187156a5fdf0324205e167f42648
parsapartovi <pparsapartovi13@gmail.com>
2025-12-05 14:04:36 +0330

    teaming up football players

---
+++ commit3.py
@@ -0,0 +1,18 @@
+import random
+
+
+players_name = ["ali", "hamid", "alireza", "nima"]
+
+
+random.shuffle(players_name)
+
+
+team1 = players_name[:2]
+team2 = players_name[2:]
+
+print("Team 1:", team1)
+print("Team 2:", team2)
+
+
+
+
